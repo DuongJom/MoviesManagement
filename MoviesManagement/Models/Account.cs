a54b8bda28ca20eb3f2beaeb315058f27a85e7c0
Duong Nguyen <ngocduong5642@gmail.com>
2024-01-11 20:54:24 +0700

    remove unused libraries

--- MoviesManagement/Models/Account.cs
+++ MoviesManagement/Models/Account.cs
@@ -1,7 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace MoviesManagement.Models
+﻿namespace MoviesManagement.Models
 {
     public class Account : BaseModel
     {
