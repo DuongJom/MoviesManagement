79eeabe066fe1a7590bc6047335c43c2ebcf41c4
Duong Nguyen <ngocduong5642@gmail.com>
2024-01-12 22:05:45 +0700

    add view

---
+++ MoviesManagement/Models/Result.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MoviesManagement.Models
+{
+    public class Result
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+    }
+}
